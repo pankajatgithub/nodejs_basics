2e2861c4bfd7e4d08218430ee27433f767c037af
Pankaj <pankaj.awasthi@novoinvent.com>
2025-03-07 16:29:38 +0530

    basic node js functionality

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const routes= require('./route');
+const { log } = require('console');
 console.log(routes.sometext);
-const server = http.createServer(routes);
+const server = http.createServer(routes.handler);
+console.log("Hi");
 
 server.listen(3000);
