ebde38be0ea5a40f4111536ac080c664ba57e74f
Pankaj <pankaj.awasthi@novoinvent.com>
2025-03-09 20:20:08 +0530

    Express routes

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.use("/add-product",(req,res,next)=>{
-    console.log("In another middleware");
-    res.send("<h1>Hello from middleware!</h1>");
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use("/",(req,res,next)=>{
-console.log("In another Express");
-res.send("<h1>Hello from Express!</h1>");
-});
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/admin',adminRoutes); // this will add filter to url, now url will be accessible after adding /admin
+app.use(shopRoutes);
+
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>');
+})
 
 app.listen(3000);
