e13ed9bfd1d1bb49f2448c76b23e0c62646e6944
Pankaj <pankaj.awasthi@novoinvent.com>
2025-03-10 15:53:46 +0530

    cleaning path name by replacing __dirname to rootDir

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,17 @@
 const path = require('path');
 const express = require("express");
 const router = express.Router();
+const rootDir = require('../util/path');
 
 router.get("/add-product",(req,res,next)=>{
     console.log("In another middleware");
-    res.sendFile(path.join(__dirname,'../','views','add-product.html')); //__dirname is global path to node js folder,'../'is one level up,views is folder name, add-product is file name to load in this path
+    // res.sendFile(path.join(__dirname,'../','views','add-product.html')); //__dirname is global path to node js folder,'../'is one level up,views is folder name, add-product is file name to load in this path
+    res.sendFile(path.join(rootDir,'views','add-product.html')); //__dirname is global path to node js folder,'../'is one level up,views is folder name, add-product is file name to load in this path
+
 });
 
 router.post("/add-product",(req,res,next)=>{
  console.log(req.body);
- 
     res.redirect("/");
     });
 
